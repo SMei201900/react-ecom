6d3136622e0711e8afcdd5844265d6d735b89f41
Sally M <smei201900@gmail.com>
2025-09-04 17:27:56 -0400

    trying to add functionality to addCart button. Console.log shows its working but still WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
 import Home from './Pages/Home.jsx';
@@ -5,12 +6,25 @@ import Cart from './Pages/Cart.jsx';
 import './App.css';
 
 function App() {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (product) => {
+  console.log('Adding to cart:', product);  //it logged 
+
+  setCartItems((prevItems) => {
+    const newCart = [...prevItems, product];
+    console.log('Updated cart items:', newCart);  //it logged 
+    return newCart;
+  });
+  };
+
+
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} /> 
-        <Route path="/cart" element={<Cart />} />
+        <Route path="/" element={<Home addToCart={addToCart} />} />
+        <Route path="/cart" element={<Cart cartItems={cartItems} />} />
       </Routes>
     </Router>
   );
--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,4 +1,3 @@
-
 const Cart = ({ cartItems }) => {
   return (
     <div>
