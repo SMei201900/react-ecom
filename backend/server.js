9bbe42d179f7dbf51e4d371f36b2b67529cdf832
Sally M <smei201900@gmail.com>
2025-09-04 17:17:12 -0400

    troubleshooting front-end to back-end connection error

--- backend/server.js
+++ backend/server.js
@@ -44,4 +44,4 @@ app.get('/', (req, res) => {
 
 // Listen for incoming requests
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, '0.0.0.0', () => console.log(`Server running on port ${port}`));
